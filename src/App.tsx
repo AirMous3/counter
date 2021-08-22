01e114b8e59bc094c4c99d13732d232984e60265
AirMous3 <airmouselol@gmail.com>
2021-08-22 16:09:27 +0300

    added some commentts

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,24 @@ import {Settings} from "./components/Settings";
 
 function App() {
 
-    let [state, setState] = useState(0)
-    let [error, setError] = useState("")
+    let [state, setState] = useState(0)  // state of main number
+    let [error, setError] = useState("") // state of error
 
     const incButton = () => {
-        setState(state + 1)
+        setState(state + 1) // callback function to increment number
     }
     const resetState = () => {
-        setState(minValue)
+        setState(minValue)   // callback function reset main number to minValue
     }
 
-    let [minValue, setMinValue] = useState(0)
-    let [maxValue, setMaxValue] = useState(0)
+    let [minValue, setMinValue] = useState(0) // minValue state
+    let [maxValue, setMaxValue] = useState(0) // maxValue state
 
     useEffect( () => {
-        let valueAsString = localStorage.getItem("counterValue")
-        let valueAsNumber = JSON.parse(valueAsString? valueAsString : "")
-        let valueAsStringMax = localStorage.getItem("counterMaxValue")
-        let valueAsNumberMax = JSON.parse(valueAsStringMax? valueAsStringMax : "")
+        let valueAsString = localStorage.getItem("counterValue")  // get Main Value as String to variable
+        let valueAsNumber = JSON.parse(valueAsString? valueAsString : "") // parse String to Number
+        let valueAsStringMax = localStorage.getItem("counterMaxValue") // get Max Value as String to variable
+        let valueAsNumberMax = JSON.parse(valueAsStringMax? valueAsStringMax : "") // parse String to Number
         let valueAsStringMin = localStorage.getItem("counterMinValue")
         let valueAsNumberMin = JSON.parse(valueAsStringMin? valueAsStringMin : "")
         setState(valueAsNumber)
@@ -33,9 +33,9 @@ function App() {
 
 
     useEffect( () => {
-    localStorage.setItem("counterValue", JSON.stringify(state))
-    localStorage.setItem("counterMinValue", JSON.stringify(minValue))
-    localStorage.setItem("counterMaxValue", JSON.stringify(maxValue))
+    localStorage.setItem("counterValue", JSON.stringify(state))  // get Main Value as String
+    localStorage.setItem("counterMinValue", JSON.stringify(minValue)) // get Min Value as String
+    localStorage.setItem("counterMaxValue", JSON.stringify(maxValue)) // get Max Value as String
     } , [state, minValue, maxValue] )
 
 
